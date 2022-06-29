aa9b3dc422bcd26fd152316e548de0648c15cb5d
Matheus Elyasha <matheuselyasha@gmail.com>
2022-06-29 16:33:08 -0300

    Hook up Search Bar to Spotify Search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -9,21 +9,58 @@ class App extends React.Component {
     super(props);
     this.state = {
       searchResults: [
-        { id: 1, name: "name 1", artist: "artist 1", album: "album 1", uri: 'spotify:track:4JZ8ONMo8AWJQx8NghGBTh' },
-        { id: 2, name: "name 2", artist: "artist 2", album: "album 2", uri: 'spotify:track:47PkkYZ45gF2eAgAkUi4I7' },
-        { id: 3, name: "name 3", artist: "artist 3", album: "album 3", uri: 'spotify:track:4tNN06VadscPBRkkCrgh0x' },
+        {
+          id: 1,
+          name: "name 1",
+          artist: "artist 1",
+          album: "album 1",
+          uri: "spotify:track:4JZ8ONMo8AWJQx8NghGBTh",
+        },
+        {
+          id: 2,
+          name: "name 2",
+          artist: "artist 2",
+          album: "album 2",
+          uri: "spotify:track:47PkkYZ45gF2eAgAkUi4I7",
+        },
+        {
+          id: 3,
+          name: "name 3",
+          artist: "artist 3",
+          album: "album 3",
+          uri: "spotify:track:4tNN06VadscPBRkkCrgh0x",
+        },
       ],
       playlistName: "playlist name",
       playlistTracks: [
-        { id: 1, name: "name 1", artist: "artist 1", album: "album 1", uri: 'spotify:track:4JZ8ONMo8AWJQx8NghGBTh' },
-        { id: 2, name: "name 2", artist: "artist 2", album: "album 2", uri: 'spotify:track:47PkkYZ45gF2eAgAkUi4I7' },
-        { id: 3, name: "name 3", artist: "artist 3", album: "album 3", uri: 'spotify:track:4tNN06VadscPBRkkCrgh0x' },
+        {
+          id: 1,
+          name: "name 1",
+          artist: "artist 1",
+          album: "album 1",
+          uri: "spotify:track:4JZ8ONMo8AWJQx8NghGBTh",
+        },
+        {
+          id: 2,
+          name: "name 2",
+          artist: "artist 2",
+          album: "album 2",
+          uri: "spotify:track:47PkkYZ45gF2eAgAkUi4I7",
+        },
+        {
+          id: 3,
+          name: "name 3",
+          artist: "artist 3",
+          album: "album 3",
+          uri: "spotify:track:4tNN06VadscPBRkkCrgh0x",
+        },
       ],
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -52,8 +89,10 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map((track) => track.uri);
-    console.log('save playlist uris')
-    console.log(trackURIs);
+  }
+
+  search(term) {
+    console.log(term);
   }
 
   render() {
@@ -64,7 +103,7 @@ class App extends React.Component {
           Ja<span className="highlight">mmm</span>ing
         </h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults
               searchResults={this.state.searchResults}
