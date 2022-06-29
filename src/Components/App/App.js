dc95578fa58f9847cbc68cc0985dfbd99d777d8f
Matheus Elyasha <matheuselyasha@gmail.com>
2022-06-29 17:27:56 -0300

    Save a User's Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,56 +4,18 @@ import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
 
+import Spotify from "../../util/Spotify"
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       searchResults: [
-        {
-          id: 1,
-          name: "name 1",
-          artist: "artist 1",
-          album: "album 1",
-          uri: "spotify:track:4JZ8ONMo8AWJQx8NghGBTh",
-        },
-        {
-          id: 2,
-          name: "name 2",
-          artist: "artist 2",
-          album: "album 2",
-          uri: "spotify:track:47PkkYZ45gF2eAgAkUi4I7",
-        },
-        {
-          id: 3,
-          name: "name 3",
-          artist: "artist 3",
-          album: "album 3",
-          uri: "spotify:track:4tNN06VadscPBRkkCrgh0x",
-        },
+       
       ],
       playlistName: "playlist name",
       playlistTracks: [
-        {
-          id: 1,
-          name: "name 1",
-          artist: "artist 1",
-          album: "album 1",
-          uri: "spotify:track:4JZ8ONMo8AWJQx8NghGBTh",
-        },
-        {
-          id: 2,
-          name: "name 2",
-          artist: "artist 2",
-          album: "album 2",
-          uri: "spotify:track:47PkkYZ45gF2eAgAkUi4I7",
-        },
-        {
-          id: 3,
-          name: "name 3",
-          artist: "artist 3",
-          album: "album 3",
-          uri: "spotify:track:4tNN06VadscPBRkkCrgh0x",
-        },
+        
       ],
     };
     this.addTrack = this.addTrack.bind(this);
@@ -89,10 +51,16 @@ class App extends React.Component {
 
   savePlaylist() {
     const trackURIs = this.state.playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackURIs).then(() => {
+      this.setState({ playlistName: 'New Playlist', playlistTracks: [] });
+    });
   }
 
   search(term) {
     console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults})
+    })
   }
 
   render() {
